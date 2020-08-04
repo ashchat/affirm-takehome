6f2ef055f5b0771a72190c5ebdced9572b462b6c
Ashish Chatterjee <chitchattering@gmail.com>
2020-08-03 17:31:15 -0700

    Initial Commit

---
+++ AffirmTakeHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AffirmTakeHome
+//
+//  Created by Ashish Chatterjee on 8/3/20.
+//  Copyright © 2020 Ashish Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
